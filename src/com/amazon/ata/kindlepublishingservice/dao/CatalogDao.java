e5c61c2379936c3f87b58f39c9dbeb6d57e8ce94
Ralph2 <academy4@talentamp.com>
2023-04-12 16:07:06 +0800

    MT1 Finished

--- src/com/amazon/ata/kindlepublishingservice/dao/CatalogDao.java
+++ src/com/amazon/ata/kindlepublishingservice/dao/CatalogDao.java
@@ -2,9 +2,12 @@ package com.amazon.ata.kindlepublishingservice.dao;
 
 import com.amazon.ata.kindlepublishingservice.dynamodb.models.CatalogItemVersion;
 import com.amazon.ata.kindlepublishingservice.exceptions.BookNotFoundException;
+import com.amazon.ata.kindlepublishingservice.publishing.KindleFormattedBook;
+import com.amazon.ata.kindlepublishingservice.utils.KindlePublishingUtils;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 import javax.inject.Inject;
@@ -38,32 +41,25 @@ public class CatalogDao {
 
         return book;
     }
-
-    /**
-     * Marks the latest version of the book from the catalog corresponding to the specified book id as inactive.
-     * Throws a BookNotFoundException if no version is found.
-     * @param bookId Id associated with the book.
-     */
     public void removeBookFromCatalog(String bookId) {
         CatalogItemVersion book = getLatestVersionOfBook(bookId);
 
-        if (book == null) {
+        if (book == null || book.isInactive()) {
             throw new BookNotFoundException(String.format("No book found for id: %s", bookId));
         }
 
         book.setInactive(true);
         dynamoDbMapper.save(book);
     }
-
     // Returns null if no version exists for the provided bookId
     private CatalogItemVersion getLatestVersionOfBook(String bookId) {
         CatalogItemVersion book = new CatalogItemVersion();
         book.setBookId(bookId);
 
         DynamoDBQueryExpression<CatalogItemVersion> queryExpression = new DynamoDBQueryExpression()
-                .withHashKeyValues(book)
-                .withScanIndexForward(false)
-                .withLimit(1);
+            .withHashKeyValues(book)
+            .withScanIndexForward(false)
+            .withLimit(1);
 
         List<CatalogItemVersion> results = dynamoDbMapper.query(CatalogItemVersion.class, queryExpression);
         if (results.isEmpty()) {
